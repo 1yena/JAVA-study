d9b6d07125912b7e5a46243640ead9894489dad8
CYN <elmocize1@naver.com>
2021-10-13 12:31:05 +0900

    21-10-13

---
+++ CH02_Variables/src/convertingTemp/App.java
@@ -0,0 +1,15 @@
+package convertingTemp;
+
+public class App {
+
+	public static void main(String[] args) {
+		// ¼·¾¾ Celsius => È­¾¾ Fahrenheit º¯È¯
+		
+		double c = 25; //¼·¾¾
+		double f = (c * 9/5) + 32; //È­¾¾
+		
+		System.out.println("¼·¾¾ " +c+ "µµ´Â È­¾¾ "+f+"µµÀÌ´Ù.");
+		
+	}
+
+}
---
+++ CH02_Variables/src/integer/App.java
@@ -0,0 +1,19 @@
+package integer;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		// 변수 만들기 : 1.변수 선언(자료형) 2.변수 초기화
+		int x;	// [int]는 정수 자료형 - 정수로 사용할 'x'를 선언함.
+		x = 7;	// 'x'의 값을 초기화함
+		int y = 10;	// 'y' 정수 선언과 동시에 초기화.
+		int z = x + y;
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z); // 한줄 복사 단축키 : ctrl + alt + 아래화살표키
+		
+	}
+
+}

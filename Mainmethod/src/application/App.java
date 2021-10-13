3b21c017420fbb070a6e6074847caa0a2b65f4c7
CYN <elmocize1@naver.com>
2021-10-13 11:18:03 +0900

    첫번째commit

---
+++ Mainmethod/src/application/App.java
@@ -0,0 +1,16 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// 메인메소드에서 프로그램 시작
+		// 자동정렬 단축키 : Ctrl + Shift + F
+		// 실행(run app) 단축키 : Ctrl + F11
+		
+		System.out.println("안녕하세요");
+		System.out.println("새 자바 프로그램을");
+		System.out.println("실행합니다.");
+		
+	}
+
+}
